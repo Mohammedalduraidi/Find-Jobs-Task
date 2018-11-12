7440f3a33f2306388395c6e15c866eb8bd188e29
Mohammedalduraidi <mohd.alduraidi@gmail.com>
2018-11-12 02:22:57 +0200

    (add) adding a routes in front end

--- find-job-client/src/app/app.module.ts
+++ find-job-client/src/app/app.module.ts
@@ -1,14 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
-
+import { LandingComponent } from './landing/landing.component';
+const appRoutes: Routes = [
+  { path: '', component: LandingComponent },
+];
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LandingComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: false } // <-- debugging purposes only
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
