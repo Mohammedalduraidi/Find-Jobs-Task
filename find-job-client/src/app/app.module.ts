6e0912f42cfaaf00d31b1fbe83f6127a79a7cd53
Mohammedalduraidi <mohd.alduraidi@gmail.com>
2018-11-14 02:32:01 +0200

    (add) adding a login and signup modal in the landing page

--- find-job-client/src/app/app.module.ts
+++ find-job-client/src/app/app.module.ts
@@ -1,22 +1,72 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+
 import { AppComponent } from './app.component';
-import { LandingComponent } from './landing/landing.component';
+import { LandingComponent, LoginModal, SignupModal } from './landing/landing.component';
+
+import { HomeComponent } from './home/home.component';
+import { ResetPasswordComponent } from './reset-password/reset-password.component';
+import { BrowserAnimationsModule, NoopAnimationsModule } from '@angular/platform-browser/animations';
+
+
+// import angular material 
+import {
+  MatButtonModule,
+  MatFormFieldModule,
+  MatInputModule,
+  MatRippleModule,
+  MatDialogModule,
+  MatSelectModule,
+} from '@angular/material';
+
+
+
 const appRoutes: Routes = [
+  { path: 'Home', component: HomeComponent },
+  { path: 'reset', component: ResetPasswordComponent },
+
+
   { path: '', component: LandingComponent },
 ];
 @NgModule({
   declarations: [
     AppComponent,
-    LandingComponent
+    LandingComponent,
+    HomeComponent,
+    ResetPasswordComponent,
+    LoginModal,
+    SignupModal
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     RouterModule.forRoot(
       appRoutes,
       { enableTracing: false } // <-- debugging purposes only
-    )
+    ),
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatRippleModule,
+    MatDialogModule,
+    BrowserAnimationsModule,
+    NoopAnimationsModule,
+    MatSelectModule
+  ],
+  exports: [
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatRippleModule,
+    MatDialogModule,
+    BrowserAnimationsModule,
+    NoopAnimationsModule,
+    MatSelectModule
+  ], entryComponents: [
+    LoginModal,
+    SignupModal,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- find-job-client/src/app/landing/landing.component.ts
+++ find-job-client/src/app/landing/landing.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject, ViewChild } from '@angular/core';
+import { MatDialog, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-landing',
@@ -7,9 +9,64 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LandingComponent implements OnInit {
 
-  constructor() { }
+  constructor(public dialog: MatDialog) { }
 
   ngOnInit() {
+  }
+  openModal = () => {
+    this.dialog.open(LoginModal);
+  }
+  openSignup = () => {
+    this.dialog.open(SignupModal);
+  }
+}
+@Component({
+  selector: 'login-Modal',
+  templateUrl: 'login.html',
+})
+export class LoginModal {
+  acctypes = [
+    { value: 'bm', viewValue: 'Business Maneger' },
+    { value: 'pm', viewValue: 'Project Manager' }
+  ]
+  access = ''
+
+  constructor(
+    public dialogRef: MatDialogRef<LandingComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: LoginModal,
+    private router: Router) { }
+
+  cancel = (): void => {
+    this.dialogRef.close();
+  }
+
+  onSelect(element = '') {
+    console.log("jackel is here", element)
+  }
+
+}
+@Component({
+  selector: 'signup-Modal',
+  templateUrl: 'signup.html',
+})
+export class SignupModal {
+  acctypes = [
+    { value: 'bm', viewValue: 'Business Maneger' },
+    { value: 'pm', viewValue: 'Project Manager' }
+  ]
+  access = ''
+
+  constructor(
+    public dialogRef: MatDialogRef<LandingComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: SignupModal,
+    private router: Router) { }
+
+  cancel = (): void => {
+    this.dialogRef.close();
+  }
+
+  onSelect(element = '') {
+    console.log("jackel is here", element)
   }
 
 }
